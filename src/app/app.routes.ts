67c68e3ea985f9515645f65e76f6655802b2fa95
Dawid Garncarz <dawid.garncarz1@gmail.com>
2024-11-11 12:10:39 +0100

    Forgot about comma.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { CartComponent } from './cart/cart.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
-import {ResetPasswordComponent} from './auth/reset-password/reset-password.component';
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 
 export const routes: Routes = [
   {
@@ -28,7 +28,7 @@ export const routes: Routes = [
   {
     path: 'reset-password/:resetToken',
     component: ResetPasswordComponent,
-  }
+  },
   {
     path: 'books',
     component: HomeComponent,
