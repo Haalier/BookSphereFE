aa08156f849dbb81957c54fbf1b514d6e9f090e8
Dawid Garncarz <dawid.garncarz1@gmail.com>
2025-01-13 15:03:12 +0100

    Added functionality to save login in browser's local storage to make logging in easier.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,47 +1,64 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
 } from '@angular/forms';
-import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
-import { ErrorData, ErrorService } from '../../services/error.service';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
+import {ErrorData, ErrorService} from '../../services/error.service';
+import {debounceTime} from 'rxjs';
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [ReactiveFormsModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+    selector: 'app-login',
+    standalone: true,
+    imports: [ReactiveFormsModule],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
-  authService = inject(AuthService);
-  errorService = inject(ErrorService);
-  router = inject(Router);
-  errMsg: ErrorData | null = null;
+    authService = inject(AuthService);
+    errorService = inject(ErrorService);
+    router = inject(Router);
+    errMsg: ErrorData | null = null;
 
-  ngOnInit() {
-    this.errorService.error$.subscribe((err) => {
-      this.errMsg = err;
+    ngOnInit() {
+        const savedLogin = window.localStorage.getItem('saved-login');
+
+        if (savedLogin) {
+            const loadedLogin = JSON.parse(savedLogin);
+            this.loginForm.patchValue({
+                email: loadedLogin.email,
+            })
+        }
+
+        this.errorService.error$.subscribe((err) => {
+            this.errMsg = err;
+        });
+
+        this.loginForm.valueChanges.pipe(debounceTime(500)).subscribe({
+            next: value => {
+                window.localStorage.setItem('saved-login', JSON.stringify(value));
+            }
+        })
+
+    }
+
+    loginForm = new FormGroup({
+        email: new FormControl('', {
+            validators: [Validators.required, Validators.email],
+        }),
+        password: new FormControl('', {
+            validators: [Validators.required, Validators.minLength(7)],
+        }),
     });
-  }
 
-  loginForm = new FormGroup({
-    email: new FormControl('', {
-      validators: [Validators.required, Validators.email],
-    }),
-    password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(7)],
-    }),
-  });
+    onPasswordForgot() {
+        this.router.navigate(['/forgot-password']);
+    }
 
-  onPasswordForgot() {
-    this.router.navigate(['/forgot-password']);
-  }
-
-  loginSubmit() {
-    this.authService.login(this.loginForm.value).subscribe({});
-  }
+    loginSubmit() {
+        this.authService.login(this.loginForm.value).subscribe({});
+    }
 }
