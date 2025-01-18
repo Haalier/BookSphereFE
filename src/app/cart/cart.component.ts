e1ffcc898b0affc0de0451f1f6cec6a96d70f204
Dawid Garncarz <dawid.garncarz1@gmail.com>
2025-01-18 17:47:52 +0100

    Working on cart component with working checkout

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,71 +1,89 @@
 import {
-  Component,
-  ElementRef,
-  inject,
-  OnInit,
-  ViewChild,
+    Component,
+    ElementRef,
+    inject,
+    OnInit,
+    ViewChild,
 } from '@angular/core';
-import { CartItems, CartService } from '../services/cart.service';
-import { CurrencyPipe } from '@angular/common';
-import { finalize } from 'rxjs';
+import {CartItems, CartService} from '../services/cart.service';
+import {CurrencyPipe} from '@angular/common';
+import {finalize} from 'rxjs';
+import {OrdersService} from '../services/orders.service';
+import {AuthService} from '../services/auth.service';
 
 @Component({
-  selector: 'app-cart',
-  standalone: true,
-  imports: [CurrencyPipe],
-  templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss',
+    selector: 'app-cart',
+    standalone: true,
+    imports: [CurrencyPipe],
+    templateUrl: './cart.component.html',
+    styleUrl: './cart.component.scss',
 })
 export class CartComponent implements OnInit {
-  cartService = inject(CartService);
-  cartItems: CartItems[] = [];
-  totalPrice = 0;
+    cartService = inject(CartService);
+    ordersService = inject(OrdersService);
+    authService = inject(AuthService);
+    cartItems: CartItems[] = [];
+    totalPrice = 0;
+    user$: any;
 
-  ngOnInit() {
-    this.loadCart();
-  }
-
-  loadCart() {
-    this.cartService.getCart().subscribe((data) => {
-      this.cartItems = data.data.items;
-      this.totalPrice = data.data.total;
-    });
-  }
-
-  updateQuantity(item: any, isIncrease: boolean) {
-    const bookId = item.book.id;
-    let itemQuantity = item.quantity;
-    const updatedQuantity = isIncrease ? ++itemQuantity : --itemQuantity;
-    console.log(updatedQuantity);
-
-    if (updatedQuantity <= 0) {
-      if (confirm('Are you sure you want to delete this book?')) {
-        return this.deleteFromCart(bookId);
-      }
-      return;
+    ngOnInit() {
+        this.loadCart();
+        this.authService.user$.subscribe(user => {
+            this.user$ = user;
+        })
     }
 
-    this.cartService
-      .updateCartItems(bookId, { quantity: updatedQuantity })
-      .subscribe((res) => {
-        this.loadCart();
-      });
-  }
+    loadCart() {
+        this.cartService.getCart().subscribe((data) => {
+            this.cartItems = data.data.items;
+            this.totalPrice = data.data.total;
+        });
+    }
 
-  decreaseQuantity(item: any) {
-    this.updateQuantity(item, false);
-  }
+    updateQuantity(item: any, isIncrease: boolean) {
+        const bookId = item.book.id;
+        let itemQuantity = item.quantity;
+        const updatedQuantity = isIncrease ? ++itemQuantity : --itemQuantity;
+        console.log(updatedQuantity);
 
-  increaseQuantity(item: any) {
-    this.updateQuantity(item, true);
-  }
+        if (updatedQuantity <= 0) {
+            if (confirm('Are you sure you want to delete this book?')) {
+                return this.deleteFromCart(bookId);
+            }
+            return;
+        }
 
-  deleteFromCart(_id: string) {
-    this.cartService
-      .removeFromCart(_id)
-      .pipe(finalize(() => this.loadCart()))
-      .subscribe();
-  }
+        this.cartService
+            .updateCartItems(bookId, {quantity: updatedQuantity})
+            .subscribe((res) => {
+                this.loadCart();
+            });
+    }
 
-  protected readonly Object = Object;
+    decreaseQuantity(item: any) {
+        this.updateQuantity(item, false);
+    }
+
+    increaseQuantity(item: any) {
+        this.updateQuantity(item, true);
+    }
+
+    deleteFromCart(_id: string) {
+        this.cartService
+            .removeFromCart(_id)
+            .pipe(finalize(() => this.loadCart()))
+            .subscribe();
+    }
+
+    protected readonly Object = Object;
+
+    onCheckout() {
+    //     const checkoutData = {
+    //         user: this.user$._id,
+    //         items: this.cartItems,
+    //         total: this.totalPrice,
+    //     }
+    //     this.ordersService.checkout(checkoutData).subscribe(res => {
+    //     })
+    }
 }
