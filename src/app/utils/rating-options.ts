e3b27e57452db205d2174462b989b5b4a4dd55a3
Dawid Garncarz <dawid.garncarz1@gmail.com>
2025-01-22 12:58:00 +0100

    Created external variables to hold categories and options for rating input.

---
+++ src/app/utils/rating-options.ts
@@ -0,0 +1,8 @@
+export const RATING_OPTIONS = [
+    {value: '', label: 'Any rating'},
+    {value: 1, label: '1 star or higher'},
+    {value: 2, label: '2 stars or higher'},
+    {value: 3, label: '3 stars or higher'},
+    {value: 4, label: '4 stars or higher'},
+    {value: 5, label: '5 stars only'},
+]
